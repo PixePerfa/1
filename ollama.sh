aa91b4381f4141d5fa45c16753ac431b420782e1
luke <pixelium.perfecto@gmail.com>
2024-05-28 06:00:38 +0100

    Update ollama.sh

--- ollama.sh
+++ ollama.sh
@@ -1,6 +1,6 @@
 # Install Go
-wget https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz
-sudo tar -xvf go1.20.5.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+sudo tar -xvf go1.22.3.linux-amd64.tar.gz
 sudo mv go /usr/local
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 source ~/.bashrc
